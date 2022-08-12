425c2540e2e4dc8d1d8bb403094dd8bc7357b02c
Миша Бабенко <misha77b@gmail.com>
2022-08-12 13:57:01 +0300

    server is working, but mongoDB is not connected to save info

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const config = require('config');
-const Superhero = require('./models/superhero')
+
+const superheroRouter = require('./routes/superheroes');
 
 
 const app = express();
 app.use(cors());
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // DB Config
@@ -23,18 +24,27 @@ mongoose
   .then(() => console.log('MongoDB Connected'))
   .catch((err) => console.log(err));
 
-app.get('/superheroes', function (req, res) {
-  res.send(superheroes);
-});
 
-app.post('/superheroes', function (req, res) {
-  const { nickname, real_name, origin_description, superpowers, catch_phrase, Images } = req.body;
-  const superhero = new Superhero ({ nickname, real_name, origin_description, superpowers, catch_phrase, Images });
-  superhero
-    .save()
-    .then((data) => res.send(data))
-    .catch((error) => console.log(error))
-});
+// Use Routes
+// test app get request
+app.use('/superheroes', superheroRouter)
+
+// app.use('/superheroes', superheroRouter);
+
+
+
+// app.get('/superheroes', function (req, res) {
+//   res.send(superheroes);
+// });
+
+// app.post('/superheroes', function (req, res) {
+//   const { nickname, real_name, origin_description, superpowers, catch_phrase, Images } = req.body;
+//   const superhero = new Superhero ({ nickname, real_name, origin_description, superpowers, catch_phrase, Images });
+//   superhero
+//     .save()
+//     .then((data) => res.send(data))
+//     .catch((error) => console.log(error))
+// });
 
 app.listen(PORT, function () {
   console.log(`Listening on ${PORT}`);
